d6414d496631ffa1748aa5256db5937eb776f8fc
Giada-Ripamonti <ripamontigiada@gmail.com>
2022-06-05 17:54:15 +0200

    dark-mode-commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 const changeToggle = document.querySelector('#flexSwitchCheckDefault');
-const bodyBkg = document.querySelector('body');
+const bodyBkg = document.querySelector('main');
+const navBkg = document.querySelector('nav');
 let sun = document.querySelector('#sun');
 let moon = document.querySelector('#moon')
 
 changeToggle.addEventListener('change', function() {
   if (this.checked) {
-    bodyBkg.style.backgroundColor = '#343a40';
+    bodyBkg.classList.add('dark-theme');
+    navBkg.classList.remove('navbar-light');
+    navBkg.classList.add('navbar-dark');
+    navBkg.classList.remove('bg-light');
+    navBkg.classList.add('bg-dark');
     moon.style.visibility = 'visible';
     sun.style.visibility = 'hidden';
   } else {
-    bodyBkg.style.backgroundColor = 'white';
+    bodyBkg.classList.remove('dark-theme');
+    navBkg.classList.add('navbar-light');
+    navBkg.classList.remove('navbar-dark');
+    navBkg.classList.add('bg-light');
+    navBkg.classList.remove('bg-dark');
     moon.style.visibility = 'hidden';
     sun.style.visibility = 'visible';
+    
   }
 });
