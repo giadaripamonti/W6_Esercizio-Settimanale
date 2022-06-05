67d3d919d36cbdc1668ed45b0116323d93da50b9
Giada-Ripamonti <ripamontigiada@gmail.com>
2022-06-05 23:44:21 +0200

    last-commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 const changeToggle = document.querySelector('#flexSwitchCheckDefault');
 const bodyBkg = document.querySelector('main');
 const navBkg = document.querySelector('nav');
-let sun = document.querySelector('#sun');
-let moon = document.querySelector('#moon')
 
 changeToggle.addEventListener('change', function() {
   if (this.checked) {
@@ -11,16 +9,11 @@ changeToggle.addEventListener('change', function() {
     navBkg.classList.add('navbar-dark');
     navBkg.classList.remove('bg-light');
     navBkg.classList.add('bg-dark');
-    moon.style.visibility = 'visible';
-    sun.style.visibility = 'hidden';
   } else {
     bodyBkg.classList.remove('dark-theme');
     navBkg.classList.add('navbar-light');
     navBkg.classList.remove('navbar-dark');
     navBkg.classList.add('bg-light');
     navBkg.classList.remove('bg-dark');
-    moon.style.visibility = 'hidden';
-    sun.style.visibility = 'visible';
-    
   }
 });
